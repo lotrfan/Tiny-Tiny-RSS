b89b6be88439844dcfc7d899a9c2c3e96b5f578c
Jeffrey Tolar <tolar.jeffrey@gmail.com>
2013-04-09 01:41:00 -0500

    Include XmlLint

---
+++ plugins/ff_xmllint/init.php
@@ -0,0 +1,39 @@
+<?php
+class Ff_XmlLint extends Plugin {
+
+	function about() {
+		return array(1.0,
+			"XmlLint",
+			"cheetah@fastcat.org",
+			false);
+	}
+
+	function init($host) {
+		$host->add_hook($host::HOOK_FEED_FETCHED, $this);
+	}
+	
+	function hook_feed_fetched($feed_data) {
+		$descriptorspec = array(
+			0 => array("pipe", "r"),
+			1 => array("pipe", "w"),
+			2 => array("file", "/dev/null", "a")
+		);
+		
+		$process = proc_open('xmllint --recover -', $descriptorspec, $pipes);
+		
+		if (is_resource($process)) {
+			fwrite($pipes[0], $feed_data);
+			fclose($pipes[0]);
+			$new_feed_data = stream_get_contents($pipes[1]);
+			fclose($pipes[1]);
+			
+			$return_value = proc_close($process);
+			if ($return_value == 0)
+				$feed_data = $new_feed_data;
+		}
+		
+		return $feed_data;
+	}
+
+}
+?>
